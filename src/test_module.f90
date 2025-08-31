599c1f04475b8bc3fdc3c22fbc75e0b4b437f4dd
Christopher Albert <albert@tugraz.at>
2025-08-31 19:47:34 +0200

    cleanup: remove test artifacts from main project directories

--- src/test_module.f90
+++
@@ -1,9 +0,0 @@
-module test_module
-    implicit none
-contains
-    function add_numbers(a, b) result(sum)
-        integer, intent(in) :: a, b
-        integer :: sum
-        sum = a + b
-    end function add_numbers
-end module test_module
