e5fcdd9e7d222d230d1a18e29651c38addfa79a9
Christopher Albert <albert@tugraz.at>
2026-01-07 16:36:27 +0100

    test: enforce gcov flag assertions

--- test/test_gcov_flag_behavior_1250.f90
+++ test/test_gcov_flag_behavior_1250.f90
@@ -9,14 +9,18 @@ program test_gcov_flag_behavior_1250
     type(config_t) :: config
     character(len=:), allocatable :: found_files(:)
     logical :: should_attempt
+    integer :: failures
 
     call initialize_default_config(config)
+
+    failures = 0
 
     should_attempt = should_attempt_gcov_generation(config, found_files)
     if (.not. should_attempt) then
         write (output_unit, '(A)') '  [PASS] default auto-discovery skips gcov'
     else
         write (output_unit, '(A)') '  [FAIL] default auto-discovery triggered gcov'
+        failures = failures + 1
     end if
 
     config%auto_discovery = .false.
@@ -25,6 +29,7 @@ program test_gcov_flag_behavior_1250
         write (output_unit, '(A)') '  [PASS] --gcov requests generation'
     else
         write (output_unit, '(A)') '  [FAIL] --gcov did not request generation'
+        failures = failures + 1
     end if
 
     allocate (character(len=1) :: found_files(1))
@@ -34,5 +39,10 @@ program test_gcov_flag_behavior_1250
         write (output_unit, '(A)') '  [PASS] existing gcov skips generation'
     else
         write (output_unit, '(A)') '  [FAIL] existing gcov retriggered generation'
+        failures = failures + 1
+    end if
+
+    if (failures > 0) then
+        error stop 1
     end if
 end program test_gcov_flag_behavior_1250
